837829edfea527a574409baa8c37d022071ca1e6
David Phares <dphares@gmail.com>
2020-05-14 11:21:12 +0200

    Documentation and texts in English added

--- SudokuGoku.py
+++ SudokuGoku.py
@@ -4,6 +4,15 @@ import tabulate as tb
 import time
 
 class SudokuSolver:
+    """
+    Sudoku Solver class
+
+    Parameters:
+    sudoku (pandas or numpy array): Sudoku in either pandas or numpy array format
+
+    Returns:
+    None
+    """
 
     _debug = False
 
@@ -23,32 +32,53 @@ class SudokuSolver:
     def __init__(self, sudoku):
 
         if not isinstance(sudoku, pd.DataFrame) and not isinstance(sudoku, np.ndarray):
-            raise ValueError("The sudoku must be either a Pandas or a Numpy Array")
+            raise ValueError("Sudoku must be either a Pandas or a Numpy Array")
 
         if isinstance(sudoku, np.ndarray):
             self._sudoku = pd.DataFrame(sudoku)
         else:
             self._sudoku = sudoku
 
-        self._initial_sudoku = self._sudoku.copy()
+        self._original = self._sudoku.copy()
 
         if not self.__check_sudoku():
             raise ValueError("Initial sudoku is invalid")
 
-    def draw_sudoku(self):
-        '''INFO: Draws the sudoku'''
+    def draw_sudoku(self, original = False):
+        """Draws the sudoku in a grid (original or solution)
+
+        Parameters:
+        original (boolean): Indicates whether the sudoku to be printed is the original. Default value is False.
+
+        Returns:
+        None
+
+        """
 
         printable = np.empty([9, 9], dtype="str")
 
+        if original:
+            sudoku = self._original
+        else:
+            sudoku = self._sudoku
+
         for row in range(0, 9):
             for col in range(0, 9):
-                if self._sudoku.T[row][col] != 0:
-                    printable[row][col] = str(self._sudoku.T[row][col])
+                if sudoku.T[row][col] != 0:
+                    printable[row][col] = str(sudoku.T[row][col])
 
         print(tb.tabulate(printable, tablefmt="grid"))
 
     def __is_correct(self, check_set):
-        # Checks whether the set of 9 elements doesn't contain any repeated number
+        """INTERNAL: Checks whether the set of 9 elements provided doesn't contain any repeated number
+
+        Parameters:
+        check_set (array): Array with 9 numbers from a row, column or square in a Sudoku
+
+        Returns:
+        boolean: True if the set is correct, False otherwise
+
+        """
 
         unique, counts = np.unique(check_set, return_counts=True)
 
@@ -62,7 +92,15 @@ class SudokuSolver:
         return True
 
     def __check_sudoku(self):
-        # Check rows, columns and squares
+        """INTERNAL: Check rows, columns and squares
+
+        Parameters:
+        None
+
+        Returns:
+        boolean: True if the Sudoku is correct, False otherwise
+
+        """
 
         for i in range(0, 9):
 
@@ -72,9 +110,28 @@ class SudokuSolver:
         return True
 
     def __is_complete(self):
+        """INTERNAL: Check rows, columns and squares
+
+        Parameters:
+        None
+
+        Returns:
+        boolean: True if the Sudoku is correct, False otherwise
+
+        """
+
         return self._sudoku.to_numpy().sum() == (45 * 9)
 
     def __next_empty(self):
+        """INTERNAL: Gets the next gap in the sudoku to be filled
+
+        Parameters:
+        None
+
+        Returns:
+        row, col: Row and column in the sudoku to be filled
+
+        """
 
         for row in range(0, 9):
             for col in range(0, 9):
@@ -84,36 +141,45 @@ class SudokuSolver:
         return -1, -1
 
     def __fill_sudoku_v1(self, n=1):
+        """INTERNAL: Solves sudoku recursively checking every value from 1 to 9 for each empty cell
 
-        # Si el sudoku está completado, lo devuelvo
+        Parameters:
+        n (int): OPTIONAL. Defines the iteration for debug purposes
+
+        Returns:
+        boolean: True if the Sudoku is correct, False otherwise
+
+        """
+
+        # If the Sudoku is already solved, return True
 
         if self.__is_complete():
 
             if self._debug:
-                print(f"¡El sudoku está terminado en la iteración {n}!")
+                print(f"Sudoku solved in the iteration {n}!")
 
             return True
 
-        # Si no, busco el siguiente elemento vacío y lo empiezo a llenar
+        # Look for the next gap to fill
 
         row, col = self.__next_empty()
 
         if self._debug:
-            print(f"Iteración {n}: Vamos a por el hueco de ({row}, {col}).")
+            print(f"Iteration {n}: Lets fill the gap ({row}, {col}).")
 
         for i in range(1, 10):
 
-            # Probamos el valor
+            # Check the value
 
             if self._debug:
-                print(f"Iteración {n}: Probamos con el valor {i} y comprobamos.")
+                print(f"Iteration {n}: Checking the value {i}.")
 
             self._sudoku[row][col] = i
 
             if self.__check_sudoku():
 
                 if self._debug:
-                    print(f"Iteración {n}: El sudoku parece válido. Pasamos a buscar el siguiente valor")
+                    print(f"Iteration {n}: The sudoku seems valid. Move to the next gap.")
 
                 if self.__fill_sudoku_v1(n + 1):
                     return True
@@ -123,6 +189,15 @@ class SudokuSolver:
         return False
 
     def __get_options(self):
+        """INTERNAL: Get the different options for each cell in the sudoku based on the existing values in row, column and square where the cell is.
+
+        Parameters:
+        None
+
+        Returns:
+        array: Possible values for the cell
+
+        """
 
         options = pd.DataFrame([], index=np.arange(0, 9), columns=np.arange(9))
 
@@ -133,23 +208,23 @@ class SudokuSolver:
                     options[row][col] = [self._sudoku[row][col]]
                 else:
                     if self._debug:
-                        print(f"Analizando ({row}, {col}) --> {self._sudoku[row][col]}")
+                        print(f"Analyzing ({row}, {col}) --> {self._sudoku[row][col]}")
 
                     options_base = list(range(1, 10))
 
                     # Remove options in col
 
                     if self._debug:
-                        print("\tAnálisis 1: ", self._sudoku.T[col].to_numpy())
+                        print("\tAnalysis 1: ", self._sudoku.T[col].to_numpy())
 
                     for value in self._sudoku.T[col]:
                         if value in options_base:
                             options_base.remove(value)
 
-                            # Remove options in row
+                    # Remove options in row
 
                     if self._debug:
-                        print("\tAnálisis 2: ", self._sudoku[row].to_numpy())
+                        print("\tAnalysis 2: ", self._sudoku[row].to_numpy())
 
                     for value in self._sudoku[row]:
                         if value in options_base:
@@ -158,9 +233,8 @@ class SudokuSolver:
                     # Remove options in square
 
                     if self._debug:
-                        print(f"\tEl bloque es el {(int(col / 3) * 3 + (int(row / 3) + 1))}.")
-                        print("\tAnálisis cuadro: ",
-                              np.unique(np.where(self._squares == int(col / 3) * 3 + (int(row / 3) + 1), self._sudoku, 0)))
+                        print(f"\tBlock is {(int(col / 3) * 3 + (int(row / 3) + 1))}.")
+                        print("\tAnalysis square: ", np.unique(np.where(self._squares == int(col / 3) * 3 + (int(row / 3) + 1), self._sudoku, 0)))
 
                     for value in np.unique(np.where(self._squares == int(col / 3) * 3 + (int(row / 3) + 1), self._sudoku, 0)):
                         if value in options_base:
@@ -171,13 +245,23 @@ class SudokuSolver:
         return options
 
     def __fill_sudoku_v2(self, options, n=1):
+        """INTERNAL: Solves sudoku recursively checking only the possible values for each gap according to the position in the grid
+
+        Parameters:
+        options (array): Matrix with the options for each cell
+        n (int): OPTIONAL. Iteration for debug purposes
+
+        Returns:
+        boolean: True if the Sudoku is correct, False otherwise
+
+        """
 
         # Si el sudoku está completado, lo devuelvo
 
         if self.__is_complete():
 
             if self._debug:
-                print(f"¡El sudoku está terminado en la iteración {n}!")
+                print(f"Sudoky solved in iteration {n}!")
 
             return True
 
@@ -186,14 +270,14 @@ class SudokuSolver:
         row, col = self.__next_empty()
 
         if self._debug:
-            print(f"Iteración {n}: Vamos a por el hueco de ({row}, {col}). Las opciones son {options[row][col]}")
+            print(f"Iteration {n}: Lets fill the gap ({row}, {col}). Options are {options[row][col]}")
 
         for i in options[row][col]:
 
-            # Probamos el valor
+            # Check the value
 
             if self._debug:
-                print(f"Iteración {n}: Probamos con el valor {i} y comprobamos.")
+                print(f"Iteration {n}: Checking value {i}.")
 
             self._sudoku[row][col] = i
 
@@ -203,19 +287,30 @@ class SudokuSolver:
             if self.__check_sudoku():
 
                 if self._debug:
-                    print(f"Iteración {n}: El sudoku parece válido. Pasamos a buscar el siguiente valor")
+                    print(f"Iteration {n}: Sudoku seems valid. Lets fill the next gap.")
 
                 if self.__fill_sudoku_v2(options, n + 1):
                     return True
 
         if self._debug:
-            print(f"Iteración {n}: Ninguna opción es válida. Volvemos a la iteración anterior")
+            print(f"Iteration {n}: No options valid. Back to previous iteration.")
 
         self._sudoku[row][col] = 0
 
         return False
 
     def solve_sudoku(self, method):
+        """Solves sudoku using the method provided
+
+        Parameters:
+        method (str): Method to be used for solving the sudoku. Options are:
+            * brute: Check every value from 1 to 9
+            * options: Check only possible values for each cell
+
+        Returns:
+        boolean: True if the Sudoku is solved, False otherwise
+
+        """
 
         if method not in ("brute", "options"):
             raise ValueError("Method must be either 'brute' or 'options'")
